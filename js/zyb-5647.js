c36bdc82a2ee652e9fd529a3b626e66d87ea2d6a
fernandakaory <rm551104@fiap.com.br>
2023-04-18 11:08:11 -0300

    Estrutura inicial para o ex de funções

--- js/zyb-5647.js
+++ js/zyb-5647.js
@@ -53,41 +53,41 @@
 
 
 
-//Declaração de arrays
-let nr1=[1,2,3,4,5];
-let nr2=[6,7,8,9,10];
-//spalhando o array com SPREAD[...]
-let nr3=[...nr1,...nr2]
+// //Declaração de arrays
+// let nr1=[1,2,3,4,5];
+// let nr2=[6,7,8,9,10];
+// //spalhando o array com SPREAD[...]
+// let nr3=[...nr1,...nr2]
 
-console.log(`Array-1 ${nr1}`);
-console.log(`Array-2 ${nr2}`);
-console.log(`Array-3 ${nr3}`);
+// console.log(`Array-1 ${nr1}`);
+// console.log(`Array-2 ${nr2}`);
+// console.log(`Array-3 ${nr3}`);
 
-//Listando um array! Tecnica especial
-// nr3.forEach((itemDoArray)=>{
-//     console.log(`Array-3 sendo listado ${itemDoArray}`);   
-// })
+// //Listando um array! Tecnica especial
+// // nr3.forEach((itemDoArray)=>{
+// //     console.log(`Array-3 sendo listado ${itemDoArray}`);   
+// // })
 
-// 18-04
-//Listando o array com MAP
-//a cada iteração vai gerar o elemento
-//key var servir como índice e começa do xero
-nr3.map((numero,key)=>{
-    console.log(`${(key+1)} - Itens do array - ${numero}`);
+// // 18-04
+// //Listando o array com MAP
+// //a cada iteração vai gerar o elemento
+// //key var servir como índice e começa do xero
+// nr3.map((numero,key)=>{
+//     console.log(`${(key+1)} - Itens do array - ${numero}`);
     
 
 
-})
+// })
 
-//Convertendo html em array de forma indireta
-const liElementHtml = document.getElementsByTagName("li");
-const liElementArray= [...liElementHtml];
-//agora tenho uma lista de elementos html só que em array
+// //Convertendo html em array de forma indireta
+// const liElementHtml = document.getElementsByTagName("li");
+// const liElementArray= [...liElementHtml];
+// //agora tenho uma lista de elementos html só que em array
 
-liElementArray.map((item,key)=>{
-    if(item.textContent=="Item=39"){
-        console.log(`${key+1} - ${item.textContent="LOCALIZADO"}`)  
-    }
-})
-//console.log(liElementArray);
-//mostra o array
+// liElementArray.map((item,key)=>{
+//     if(item.textContent=="Item=39"){
+//         console.log(`${key+1} - ${item.textContent="LOCALIZADO"}`)  
+//     }
+// })
+// //console.log(liElementArray);
+// //mostra o array
